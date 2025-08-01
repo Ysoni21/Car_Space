9efea67ce8cd2b0a6bc90888a6b0d212019be39c
Ysoni21 <soniyash1298@gmail.com>
2025-08-01 13:36:25 +0530

    update

--- my-test-site/src/components/about.js
+++ my-test-site/src/components/about.js
@@ -4,7 +4,7 @@ function about(){
     return(
         <section className="About">
             <h2>About me</h2>
-            <p>This is my first site learning React</p>
+            <p>This is my first site learning React.</p>
         </section>
     );
 }
